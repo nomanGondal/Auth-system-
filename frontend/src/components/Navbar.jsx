4a9d3d451ad2e77ea11aab33ba6846d7521c217b
nomanGondal <anotp76@gmail.com>
2025-08-15 16:37:13 +0500

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Navbar.css"; // Assuming you have a CSS file for styling
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
-
+const navigate = useNavigate();
   const HandleLogout = () => {
     // Clear the token from localStorage
     const confirmLogout = window.confirm("Are you sure you want to log out?");
     if (confirmLogout) {
     localStorage.removeItem("token");
     // Redirect to the login page
-    window.location.href = "/login"; // Redirect to login page
+    navigate('/login'); // Redirect to login page
   }
 }
   return (
